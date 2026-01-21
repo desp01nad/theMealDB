fdac0570411e4f321a2efc80c5190358a4a74864
desp01nad <desppoina.dim98@gmail.com>
2026-01-21 19:01:41 +0200

    Add RandomMealView

--- meal-lab-app/src/main/java/gr/unipi/ddim/meallabapp/views/RandomMealView.java
+++ meal-lab-app/src/main/java/gr/unipi/ddim/meallabapp/views/RandomMealView.java
@@ -1,69 +1,62 @@
 package gr.unipi.ddim.meallabapp.views;
 
-import java.io.ByteArrayInputStream;
-
 import gr.unipi.ddim.meallabapi.api.MealClient;
-import gr.unipi.ddim.meallabapi.models.ImageSize;
 import gr.unipi.ddim.meallabapi.models.Meal;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 
-public class RandomMealView extends VBox {
-    private final MealClient client;
-    private final Label mealTitle;
-    private final ImageView mealImageView;
 
-    public RandomMealView(MealClient client) {
-        this.client = client;
+public class RandomMealView extends MealDetailsView {
+	private String lastMealId;
+	private final Button newRandomMealBtn;
+	
+	public RandomMealView(MealClient client) {
+		super(client);
+		
+		newRandomMealBtn = new Button("New Random Recipe");
+		newRandomMealBtn.setFocusTraversable(false);
+		newRandomMealBtn.setOnAction(event -> fetchAndShowNewRandomMeal());
+		
+		buttonsRow.getChildren().add(0, newRandomMealBtn);
+		
+		fetchAndShowNewRandomMeal();
+	}
+	
+	public void fetchAndShowNewRandomMeal() {
+		
+		newRandomMealBtn.setDisable(true);
+        mealTitleLabel.setText("Loading...");
+        mealImageView.setImage(null);
         
-        this.setSpacing(10);
-        this.setAlignment(Pos.CENTER);
-        
-        this.mealTitle = new Label("");
-        
-        this.mealImageView = new ImageView();
-        this.mealImageView.setFitWidth(300);
-        this.mealImageView.setPreserveRatio(true);
+        new Thread(() -> {
+            try {
+                Meal meal;
+                do {
+                    meal = client.getRandomMeal();
+                } while (meal != null
+                        && meal.getIdMeal() != null
+                        && meal.getIdMeal().equals(lastMealId));
 
-        Button randomMealBtn = new Button("Next Random Meal");
-        randomMealBtn.setOnAction(event -> fetchRandomMeal());
+                Meal finalMeal = meal;
 
-        this.getChildren().addAll(mealImageView, mealTitle, randomMealBtn);
+                javafx.application.Platform.runLater(() -> {
+                    if (finalMeal != null) {
+                        showMeal(finalMeal);
+                        lastMealId = finalMeal.getIdMeal();
+                    } else {
+                        mealTitleLabel.setText("No meal found.");
+                    }
+                    newRandomMealBtn.setDisable(false);
+                });
 
-        fetchRandomMeal();
-    }
-
-    private void fetchRandomMeal() {
-        try {
-            Meal randomMeal = client.getRandomMeal();
-            
-            if (randomMeal != null) {
-                updateUI(randomMeal);
-            } else {
-                mealTitle.setText("No meal found.");
+            } catch (Exception e) {
+                javafx.application.Platform.runLater(() -> {
+                    mealTitleLabel.setText("Error loading meal");
+                    newRandomMealBtn.setDisable(false);
+                });
             }
-        } catch (Exception e) {
-            mealTitle.setText("Error: " + e.getMessage());
-        }
-    }
-
-    private void updateUI(Meal meal) {
-        mealTitle.setText("Meal title: " + meal.getStrMeal());
-
-        try {
-            byte[] imageBytes = client.fetchMealImage(meal, ImageSize.LARGE);
-            if (imageBytes != null && imageBytes.length > 0) {
-                ByteArrayInputStream stream = new ByteArrayInputStream(imageBytes);
-                Image image = new Image(stream);
-                mealImageView.setImage(image);
-            }
-        } catch (Exception e) {
-            System.err.println("Could not load image: " + e.getMessage());
-            mealImageView.setImage(null);
-        }
+        }).start();
     }
 }
+	
+
+    
